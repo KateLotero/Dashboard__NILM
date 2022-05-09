ac30958962d4ca07dadeb3aaee7b0a0d07e60493
KateLotero <katerine.lotero@correounivalle.edu.co>
2022-05-08 22:13:12 -0500

    calculate power consumption by month

--- Frontend/src/views/elect/Elect.js
+++ Frontend/src/views/elect/Elect.js
@@ -9,19 +9,14 @@ import {
 
 
 
-
 const API = process.env.REACT_APP_API; //Call the environment var to connect with Flask
 console.log(API)// print server address
-
-
-
 
 const Elect = () => {
     const [powerAppliances, setPowerAppliances] = useState([])
     const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-    const elects = ['nevera','washing_machine']
-
-//***************************************  requests to the server *************************************/
+    let devices = []
+//*************************************** requests to the server *************************************/
     
     const getAppliances =  () => {      
         fetch(`${API}/allData`,) // GET is the default method
@@ -37,11 +32,11 @@ const Elect = () => {
 //*************************************** Data process **************************************************
     
     // Calculate average power consumption each day
-    function dataAppliances (data){  //Receive the data extracted of the data base
-            const dataElect = data
-            //console.log('Datos de una casa', dataElect)
+    function calcPowerDay (data){  //Receive the data extracted of the data base
+            const dataDB = data
+            //console.log('dataDB', dataDB)
 
-            const muestras =  dataElect.map(function(x){
+            const dataElects =  dataDB.map(function(x){
                 let newArray = {
                     'deviceId': x.deviceId,
                     'd': x.d,
@@ -49,57 +44,73 @@ const Elect = () => {
                 }
                 return newArray
             })
-            console.log('elect, fecha , Muestras elect', muestras)
-            const filtro = muestras.filter(x => x.d.includes('Jan'))
-            console.log('filtrosiiiiiiiiii',filtro)
-
-            /*
-
-            const samplesElect = muestras.map(x => [x.deviceId,x.d,x.samples])
+            //console.log('deviceId, d, samples', dataElects)                 
             
+            const samplesElect =  dataElects.map(x => [x.samples])            
             for (let i = 0; i < samplesElect.length; i++){
-                let sample = samplesElect[i][2]
-                console.log('muestras', sample)
+                let sample = samplesElect[i][0]
+                //console.log('muestras', sample)
 
                 let powerElect = sample.map(x => x.power)
-                console.log('potencia', powerElect) 
-                
+                //console.log('potencia', powerElect) 
+
                 let average = (powerElect.reduce( (total,value) => total+value))/96 //Preguntar a Pablo si siempre serán 96
-                console.log(average)
+                //console.log(average)
 
-                samplesElect[i][2] = average
-                console.log('promedio por día', samplesElect)
+                dataElects[i]['power'] =  average
+                    
             }
-            return samplesElect */
-        }
-  
-    const dataElects = dataAppliances(powerAppliances)
-    console.log('Datos Electrodomésticos procesados', dataElects)
-/*
-    //filter by appliance 
-    const fridge = dataElects.filter(x => x[0] == 'nevera') 
-    console.log('datos nevera',fridge)  
-
-    let fridgeMonth = []
-    let average = 0
-    for (let k = 0; k < months.length; k++){
-        
-        for (let i = 0; i < fridge.length; i++){
-            if (fridge[i][1] == months[k]){
-                average = fridge[i][1] + average
-                //console.log('promedio',average,'meses',months[k])
-            }
-            fridge.some 
-            average = (powerElect.reduce( (total,value) => total+value))
-            
-        }
-
-        fridgeMonth.push([months[k],average])          
+            return dataElects
     }
-    console.log('potencia mes nevera', fridgeMonth)
+    const dataElects = calcPowerDay(powerAppliances)
+    console.log('Datos Electrodomésticos procesados', dataElects)
 
-        
+    
+    // Calculate average power consumption per month 
+    function calcPowerMonth (dataElects){
 
+        //capture the devices available 
+        devices = dataElects.map(x => x.deviceId)
+        devices = new Set (devices)
+        console.log("devices", devices)
+         
+        let appliancesMonth = []
+        devices.map(appliance => {
+
+            //filter by appliance 
+            const dataDevice = dataElects.filter(x => x.deviceId == appliance) 
+            console.log(`datos ${appliance}`, dataDevice) 
+
+            //filter by month
+            for (let k = 0; k < months.length; k++){
+                const filterMonth = dataDevice.filter(x => x.d.includes(months[k]))
+                //console.log('filtro por mes', filterMonth)
+
+                let powerElect = filterMonth.map(x => x.power)
+                //console.log('potencia', powerElect)
+
+                let average = (powerElect.reduce( (total,value) => total+value,0))/(filterMonth.length)
+                //console.log('pr', average)
+
+                average ? appliancesMonth.push({'deviceId': appliance,'month': months[k], 'power': average}): 0 // if average is different to NaN ? haga esto : si no esto 
+                
+            }
+        })
+
+        return appliancesMonth
+    }
+    
+    const powerMonth = calcPowerMonth(dataElects)
+    console.log("Potencia de los dispos por mes", powerMonth)
+      
+    const data = {
+        datasets: [{
+            data: [10, 20],
+            backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+        }],
+
+        labels: devices
+    };
 
 
 //**************************************** page ****************************************/
@@ -111,19 +122,8 @@ const Elect = () => {
                 <CCard className="mb-4">
                 <CCardHeader>Enero</CCardHeader>
                 <CCardBody>
-                    <CChartPie
-                
-                    data={{
-                        labels: ['Nevera', 'Microondas', 'Yellow'],
-                        datasets: [
-                        {
-                            data: [300, 50, 100],
-                            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-                            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-                            
-                        },
-                        ],
-                    }}
+                    <CChartPie                                   
+                    data={data}
                 
                     
                     />
@@ -131,8 +131,8 @@ const Elect = () => {
             </CCard>
             <CCol xs={6}>
                 <CCard>
-                       
-                
+
+                                    
                 </CCard>
 
             </CCol>
